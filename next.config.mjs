c11f177370bc02f26af21237876f88988db13f67
tulsyanshubham <141614975+tulsyanshubham@users.noreply.github.com>
2024-06-10 00:14:35 +0530

    config changes

--- next.config.mjs
+++ next.config.mjs
@@ -1,5 +1,8 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
+    basePath: "/Music-Academy",
+    output: "export",
+    reactStrictMode: true,
     images: {
         domains: ['images.unsplash.com'],
     }
